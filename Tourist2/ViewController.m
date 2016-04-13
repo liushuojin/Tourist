2a9dcd652d09b4b863d66e9d20982c927152019c
liushuojin <jinkie@vip.qq.com>
2016-04-13 23:49:29 +0800

    Initial Commit

---
+++ Tourist2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Tourist2
+//
+//  Created by 刘硕晋 on 16/4/13.
+//  Copyright © 2016年 ginkee. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Tourist2UITests/Tourist2UITests.m
@@ -0,0 +1,40 @@
+//
+//  Tourist2UITests.m
+//  Tourist2UITests
+//
+//  Created by 刘硕晋 on 16/4/13.
+//  Copyright © 2016年 ginkee. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Tourist2UITests : XCTestCase
+
+@end
+
+@implementation Tourist2UITests
+
+- (void)setUp {
+    [super setUp];
+    
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    [[[XCUIApplication alloc] init] launch];
+    
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
