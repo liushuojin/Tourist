745c1b4f917142bf51596f7c5f943ae8d5db578e
liushuojin <jinkie@vip.qq.com>
2016-04-15 11:31:35 +0800

    Embed TabBarController

---
+++ Tourist2/SpotViewController.h
@@ -0,0 +1,13 @@
+//
+//  SpotViewController.h
+//  Tourist2
+//
+//  Created by 刘硕晋 on 16/4/15.
+//  Copyright © 2016年 ginkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SpotViewController : UINavigationController
+
+@end
