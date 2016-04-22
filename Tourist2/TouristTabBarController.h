1af3b1474f60a23aa7bde324e63a4ee74840e019
liushuojin <jinkie@vip.qq.com>
2016-04-22 22:17:13 +0800

    I just create a UITabBarController's subclass. I want to pass value to it.

---
+++ Tourist2/TouristTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  TouristTabBarController.h
+//  Tourist2
+//
+//  Created by 刘硕晋 on 16/4/22.
+//  Copyright © 2016年 ginkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TouristTabBarController : UITabBarController
+
+@end
---
+++ Tourist2/TouristTabBarController.m
@@ -0,0 +1,37 @@
+//
+//  TouristTabBarController.m
+//  Tourist2
+//
+//  Created by 刘硕晋 on 16/4/22.
+//  Copyright © 2016年 ginkee. All rights reserved.
+//
+
+#import "TouristTabBarController.h"
+
+@interface TouristTabBarController ()
+
+@end
+
+@implementation TouristTabBarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
