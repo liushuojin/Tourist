81d958d3fe91037a8a20863c53617dbe02520770
liushuojin <jinkie@vip.qq.com>
2016-04-23 22:13:32 +0800

    user can unfavo the spots

--- Tourist2/SpotDetailViewController.m
+++ Tourist2/SpotDetailViewController.m
@@ -93,54 +93,52 @@
 }
 
 - (void) favo{
-    
-    
-    
     NSURL* url = [NSURL URLWithString:@"http://192.168.137.1:8080/Tourist/spotFavo.spring"];
     NSString* name = self.name.text;
     //NSString* password = self.password.text;
-    
-    
     NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:url];
-    
     request.HTTPMethod = @"POST";
-    
     NSString* bodyStr = [NSString stringWithFormat:@"username=%@&spotid=%@", self.userName, self.spotId];
-    
     request.HTTPBody = [bodyStr dataUsingEncoding:NSUTF8StringEncoding];
-    
-    
-    
     [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse * _Nullable response, NSData * _Nullable data, NSError * _Nullable connectionError) {
-        
         NSLog(@"%@", data);
-        
         NSString *str = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-        
-        //self.Test.text = str;
-        
-        //self.loginText.text = [NSString stringWithFormat:@"result:%@",str];
-        
         NSLog(@"result:%@",str);
-        
         if ([str isEqualToString:@"yes"]) {
             self.favoMsg.text = @"收藏成功";
-            
             [self.isFavorite setOn:YES];
-            
-            
         } else {
-            
             //[self.isFavorite setOn:NO];
             self.favoMsg.text = @"收藏失败";
         }
-        
     }];
-    
 }
 
 - (void) noFavo{
-    self.favoMsg.text = @"取消收藏成功";
+    NSURL* url = [NSURL URLWithString:@"http://192.168.137.1:8080/Tourist/spotNoFavo.spring"];
+    NSString* name = self.name.text;
+    //NSString* password = self.password.text;
+    NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:url];
+    request.HTTPMethod = @"POST";
+    NSString* bodyStr = [NSString stringWithFormat:@"username=%@&spotid=%@", self.userName, self.spotId];
+    request.HTTPBody = [bodyStr dataUsingEncoding:NSUTF8StringEncoding];
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse * _Nullable response, NSData * _Nullable data, NSError * _Nullable connectionError) {
+        NSLog(@"%@", data);
+        NSString *str = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        NSLog(@"result:%@",str);
+        if ([str isEqualToString:@"yes"]) {
+            self.favoMsg.text = @"取消收藏成功";
+            [self.isFavorite setOn:NO];
+        } else {
+            //[self.isFavorite setOn:NO];
+            self.favoMsg.text = @"取消收藏失败";
+        }
+    }];
+    
+    
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
